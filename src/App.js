87e760c787824d672fa4985bbc23561624a1d511
brianxautumn <shred@brianautumn.com>
2020-08-01 11:57:15 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { JsWebm } from 'jswebm';
+
 function App() {
+  const [demuxer, setDemuxer] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('/video/Big_Buck_Bunny_4K.webm')
+      .then(res => res.arrayBuffer())
+      .then((buffer) => {
+        const demuxer = new JsWebm();
+        demuxer.queueData(buffer);
+        while (!demuxer.eof) {
+          demuxer.demux();
+        }
+        setDemuxer(demuxer);
+      })
+      .catch(console.log);
+  }, []);
+
+  const renderDemuxer = () => (
+    <>
+      <h3>Demuxer State:</h3>
+      <div>
+        <table className="demuxer-table">
+          <tbody>
+            <tr>
+              <th>State</th>
+              <th>Value</th>
+            </tr>
+            <tr>
+              <td>videoFormat</td>
+              <td>
+                <pre>
+                  {JSON.stringify(demuxer.videoFormat, null, 2)}
+                </pre>
+              </td>
+            </tr>
+            <tr>
+              <td>audioCodec</td>
+              <td>{demuxer.audioCodec}</td>
+            </tr>
+            <tr>
+              <td>videoPackets</td>
+              <td>{demuxer.videoPackets.length}</td>
+            </tr>
+            <tr>
+              <td>audioPackets</td>
+              <td>{demuxer.audioPackets.length}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jswebm</h1>
+      {demuxer ? renderDemuxer() : 'Loading...'}
     </div>
   );
 }
