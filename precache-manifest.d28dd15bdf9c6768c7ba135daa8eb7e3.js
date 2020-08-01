15c0e3409c7303fe575f83701a8913f3ffcfc199
brianxautumn <shred@brianautumn.com>
2020-08-01 11:59:47 -0700

    Updates

---
+++ precache-manifest.d28dd15bdf9c6768c7ba135daa8eb7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f838315028a6646afc11b0c88cb1b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38a7a23eaeb87a9d29c3",
+    "url": "/static/css/main.61d401b6.chunk.css"
+  },
+  {
+    "revision": "32a22fd9a0d376c32382",
+    "url": "/static/js/2.58d00bbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58d00bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a7a23eaeb87a9d29c3",
+    "url": "/static/js/main.4150214b.chunk.js"
+  },
+  {
+    "revision": "7f0c2340284364a5e57c",
+    "url": "/static/js/runtime-main.a1761896.js"
+  }
+]);
